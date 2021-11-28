becceece7ae158c565f3bb41b2c674a5e4cc877a
Tengbo Zou <tzou33@gatech.edu>
2021-11-27 22:46:29 -0800

    Init spring-boot template

---
+++ src/main/java/com/github/tzzzzzb/check_exchange_rate/CheckExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package com.github.tzzzzzb.check_exchange_rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckExchangeRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckExchangeRateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tzzzzzb/check_exchange_rate/CheckExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tzzzzzb.check_exchange_rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckExchangeRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
